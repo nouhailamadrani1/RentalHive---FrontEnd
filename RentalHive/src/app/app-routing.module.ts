9d7b5ef844534ff24d3866e0fe2458f7f3fadfef
Salah <salaheddinehanaoui@gmail.com>
2023-12-04 15:25:22 +0100

    added bootstrap npm to project

--- RentalHive/src/app/app-routing.module.ts
+++ RentalHive/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EquipmentsComponent } from './components/dashboard/equipments/equipments.component';
+import { Title } from '@angular/platform-browser';
+import { EstimatesComponent } from './components/dashboard/estimates/estimates.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'Equipments',
+    component : EquipmentsComponent ,
+    title : 'Equipments Page'
+  },
+  {
+    path : 'Estimates',
+    component : EstimatesComponent,
+    title : 'Estimates Page'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
