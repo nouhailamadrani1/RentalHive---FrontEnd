e2479089b39e00180a730649ed30bc813fa44cee
Salah <salaheddinehanaoui@gmail.com>
2023-12-08 09:01:36 +0100

    consumed the estimate api in the estimate service

--- RentalHive/src/app/services/EstimateService/estimate-service.service.ts
+++ RentalHive/src/app/services/EstimateService/estimate-service.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstimateServiceService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getAllEstimates(): Observable<any[]> {
+    return this.http.get<any[]>(`${environment.estimateApi}/estimate`);
+  }
+
+  getEstimateById(id: number): Observable<any> {
+    return this.http.get<any>(`${environment.estimateApi}/estimate/${id}`);
+  }
+
+  addEstimate(estimate: any): Observable<any> {
+    return this.http.post<any>(`${environment.estimateApi}/estimate`, estimate);
+  }
+
+  updateEstimate(id: number, estimate: any): Observable<any> {
+    return this.http.put<any>(`${environment.estimateApi}/estimate/${id}`, estimate);
+  }
+
+  deleteEstimate(id: number): Observable<any> {
+    return this.http.delete<any>(`${environment.estimateApi}/estimate/${id}`);
+  }
+
+  updateEstimateStatus(id: number, updatedEstimate: any, userId: number): Observable<any> {
+    return this.http.put<any>(`${environment.estimateApi}/estimate/${id}/updateStatus?userId=${userId}`, updatedEstimate);
+  }
 }
